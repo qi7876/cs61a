0f2c45f4586ea10687aa3f409011f7839966968c
qi7876 <97651950+qi7876@users.noreply.github.com>
2026-08-05 15:24:03 +0800

    lab/lab09: remove editor, complete all questions.

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,10 +1,39 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+    (cond
+        ((< num1 num2)
+            -1
+        )
+        ((= num1 num2)
+            0
+        )
+        (else
+            1
+        )
+    )
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+    (define (adder inc)
+        (+ num inc)
+    )
+    adder
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+    (define (func x)
+        (f (g x))
+    )
+    func
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+    (if (= n 0)
+        (lambda (x) x)
+        (composed f 
+            (repeat f (- n 1))
+        )
+    )
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +45,9 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b)
+    (if (= (modulo (max a b) (min a b)) 0)
+        (min a b)
+        (gcd (min a b) (modulo (max a b) (min a b)))
+    )
+)
