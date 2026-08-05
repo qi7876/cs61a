1f4cd914f4acb1c53fbc2a44970bf70cae94fc69
qi7876 <97651950+qi7876@users.noreply.github.com>
2026-08-06 01:06:22 +0800

    hw/hw08: complete all questions.

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,7 +1,50 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+    (or
+        (null? s)
+        (null? (cdr s))
+        (and
+            (<=
+                (car s)
+                (car (cdr s))
+            )
+            (ascending? (cdr s))
+        )
+    )
+)
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (cond
+        ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))
+    )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (cond
+        ((and (null? lst1) (null? lst2))
+            '()
+        )
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else
+            (cons (car lst1)
+                (cons (car lst2)
+                    (interleave (cdr lst1) (cdr lst2))
+                )
+            )
+        )
+    )
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+    (if (null? s)
+        '()
+        (cons (car s)
+            (no-repeats (filter
+                (lambda (x) (not (= (car s) x)))
+                (cdr s)
+            ))
+        )
+    )
+)
